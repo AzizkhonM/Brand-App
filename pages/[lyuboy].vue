b1c95ffc1761d8b8a1fe2ba41b8ec59b27df82b9
Azizxon <muzaffarovazizkhon@gmail.com>
2023-06-07 15:25:10 +0500

    Almost Finished

--- pages/[lyuboy].vue
+++ pages/[lyuboy].vue
@@ -7,7 +7,7 @@
     <Header />
     <div class="bg-white">
         <div class="container mx-auto h-[450px] flex justify-center items-center">
-            <img src="Error404.png" width="400" alt="">
+            <img src="/Error404.png" width="400" alt="">
             <div style="font-family: 'Orbitron'">
                 <h1 class="text-[#47B886] text-[65px]">404 not found</h1>
                 <h1 class="text-[#47B886] text-[31px] mb-[17px]">Looking something we don’t serve?</h1>
